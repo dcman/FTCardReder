c08ca8fbe9d7682cab23bf5c589c6717a3fe98fe
Justin <Justin.Johnson.90@my.csun.edu>
2014-08-28 15:41:45 -0700

    I can change text

--- src/com/example/ftcardreder/MainActivity.java
+++ src/com/example/ftcardreder/MainActivity.java
@@ -4,12 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button b1 = (Button)findViewById(R.id.button1);
+		b1.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View arg0) {
+
+				TextView text = (TextView) findViewById(R.id.textView1);
+				text.setText("Text Changed");
+			}
+		});
+
+	}
+
+
+
+
 }
