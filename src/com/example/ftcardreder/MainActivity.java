5d73e09f227339720197bb8b811ede635e17bb3b
Justin <Justin.Johnson.90@my.csun.edu>
2014-08-29 15:42:51 -0700

    Set permissions also min/max sdkV

--- src/com/example/ftcardreder/MainActivity.java
+++ src/com/example/ftcardreder/MainActivity.java
@@ -2,18 +2,18 @@ package com.example.ftcardreder;
 
 
 
+
 import android.app.Activity;
 import android.os.Bundle;
 
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-
 
 	}
 
