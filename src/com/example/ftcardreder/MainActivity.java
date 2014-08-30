805f7bdfe13a6e47e2896dd156cf32e066ca88ab
Justin <Justin.Johnson.90@my.csun.edu>
2014-08-29 17:24:40 -0700

    Descovers tags and prints basic info

--- src/com/example/ftcardreder/MainActivity.java
+++ src/com/example/ftcardreder/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.ftcardreder;
 
 
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -16,7 +24,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Intent intent = this.getIntent();
 		
+		TextView text = (TextView) findViewById(R.id.textView1);
+		text.setText(intent.toString());
+
+
 	}
 
 
