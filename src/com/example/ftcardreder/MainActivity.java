c5627f9f0c752518c03ed16b5c5c6b30e23b53d5
Justin <Justin.Johnson.90@my.csun.edu>
2014-08-29 21:40:44 -0700

    Revert 805f7bd..1adb9a7
    This rolls back to commit 805f7bdfe13a6e47e2896dd156cf32e066ca88ab.

--- src/com/example/ftcardreder/MainActivity.java
+++ src/com/example/ftcardreder/MainActivity.java
@@ -1,30 +1,38 @@
 package com.example.ftcardreder;
 
+
+
+
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.nfc.NdefMessage;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 		Intent intent = this.getIntent();
-		Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-		NdefMessage msg = (NdefMessage) rawMsgs[0];
-		extractMessage(msg);
+		
+		TextView text = (TextView) findViewById(R.id.textView1);
+		text.setText(intent.toString());
+
 
 	}
-	
-	private void extractMessage(NdefMessage msg) {
-        byte[] array = null;
-        array = msg.getRecords()[0].getPayload();
-}
-	
+
+
+
 
 }
